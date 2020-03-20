fa3976dbca0242ea5e7ed47c7a2faed4ce3cb32c
sujitbidawe <bidawesujit@gmail.com>
2020-03-21 01:13:46 +0530

    added: code to flip a coin multiple times and store output in dictionary

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,42 @@
 #!/usr/local/bin/bash
 
-randOut=$(( RANDOM%2 ))
+declare -A singletDict
 
-if [[ $randOut -eq 0 ]]
-then
-		echo "heads"
-else
-		echo "tails"
-fi
+singletDict=( ["HCount"]=0 ["TCount"]=0 )
+
+read -p "how many times do you want to flip the coin: " repeatitions
+
+# function to flip the coin
+function coinFlip() {
+
+		if [[ $(( RANDOM%2 )) -eq 0 ]]
+		then
+				flipOut=H
+		else
+				flipOut=T
+		fi
+echo $flipOut
+}
+
+# function for singlet coin flip
+function singlet(){
+for (( flipCount=1; flipCount<=repeatitions; flipCount++ ))
+do
+		flipOut="$( coinFlip $flipOut )"
+		if [[ "$flipOut" == "H" ]]
+		then
+				singletDict[HCount]=$(( ${singletDict[HCount]}+1 ))
+		else
+            singletDict[TCount]=$(( ${singletDict[TCount]}+1 ))
+		fi
+done
+
+HPercent=`echo "scale=2; (${singletDict[HCount]}*100)/$repeatitions" | bc`
+TPercent=`echo "scale=2; (${singletDict[TCount]}*100)/$repeatitions" | bc`
+
+}
+
+singlet
+echo "percentage of singlet heads: $HPercent%"
+echo "percentage of singlet tails: $TPercent%"
+
